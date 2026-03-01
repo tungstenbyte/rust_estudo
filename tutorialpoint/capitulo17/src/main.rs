429863b1840249177080259fa0a3335cc6780fb4
tungstenbyte <tungstenbyte@gmail.com>
2026-03-01 18:45:33 -0300

    boa versao iniail com I.A.

--- tutorialpoint/capitulo17/src/main.rs
+++ tutorialpoint/capitulo17/src/main.rs
@@ -52,21 +52,119 @@ fn exemplo2() {
         emp1.name, emp1.company, emp1.age
     );
 }
+
+struct Employee {
+    name: String,
+    company: String,
+    age: u32,
+}
+
 fn exemplo3() {
     println!("≡ 3 -----------------------------");
     println!("Passing a struct to a function");
+
+    let emp1 = Employee {
+        company: String::from("TutorialsPoint"),
+        name: String::from("Mohtashim"),
+        age: 50,
+    };
+    let emp2 = Employee {
+        company: String::from("TutorialsPoint"),
+        name: String::from("Kannan"),
+        age: 32,
+    };
+
+    display1(emp1);
+    display1(emp2);
 }
+
+fn display1(emp: Employee) {
+    println!(
+        "Name is :{} company is {} age is {}",
+        emp.name, emp.company, emp.age
+    );
+}
+
+fn who_is_elder(emp1: Employee, emp2: Employee) -> Employee {
+    if emp1.age > emp2.age {
+        return emp1;
+    } else {
+        return emp2;
+    }
+}
+
 fn exemplo4() {
     println!("≡ 4 -----------------------------");
-    println!("");
+    println!("Returning struct from a function");
+    let emp1 = Employee {
+        company: String::from("TutorialsPoint"),
+        name: String::from("Mohtashim"),
+        age: 50,
+    };
+    let emp2 = Employee {
+        company: String::from("TutorialsPoint"),
+        name: String::from("Kannan"),
+        age: 32,
+    };
+
+    let elder = who_is_elder(emp1, emp2);
+    println!("elder is:");
+
+    display1(elder);
 }
+
+//define dimensions of a rectangle
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+//logic to calculate area of a rectangle
+impl Rectangle {
+    fn area(&self) -> u32 {
+        //use the . operator to fetch the value of a field via the self keyword
+        self.width * self.height
+    }
+}
+
 fn exemplo5() {
     println!("≡ 5 -----------------------------");
-    println!("");
+    println!("Method in Structure");
+
+    let small = Rectangle {
+        width: 10,
+        height: 20,
+    };
+
+    println!(
+        "width is {} height is {} area of Rectangle is{}",
+        small.width,
+        small.height,
+        small.area()
+    );
 }
+
+//declare a structure
+struct Point {
+    x: i32,
+    y: i32,
+}
+impl Point {
+    //static method that creates objects of the Point structure
+    fn getInstance(x: i32, y: i32) -> Point {
+        Point { x: x, y: y }
+    }
+    //display values of the structure's field
+    fn display(&self) {
+        println!("x ={} y={}", self.x, self.y);
+    }
+}
+
 fn exemplo6() {
     println!("≡ 6 -----------------------------");
-    println!("");
+    println!("Static Method in Structure");
+    // Invoke the static method
+    let p1 = Point::getInstance(10, 20);
+    p1.display();
 }
 fn exemplo7() {
     println!("≡ 7 -----------------------------");
